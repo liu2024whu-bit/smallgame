3add866c22941ff8b9cf0fbb8eeb7003f8bc0a32
Lexin Liu <liu2024whu@gmail.com>
2026-07-15 08:19:28 +0800

    add Wuhan Nanjing chapter puzzle skeleton

---
+++ src/puzzles/university-post.js
@@ -0,0 +1,12 @@
+export const universityPostPuzzle = {
+  id: 'university-post',
+  chapter: '大三·两座城市的邮戳',
+  theme: ['武汉大学', '南京大学'],
+  difficulty: 'standard',
+  clueChain: [
+    '识别两座城市线索',
+    '旋转双邮戳',
+    '读取生日日期'
+  ],
+  answer: '1029'
+};
